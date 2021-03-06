39a786cbbb95126d9f388ec368fbf782d38214e0
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-06 11:39:13 -0700

    Draft test suites

--- test/customer-test.js
+++ test/customer-test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Customer = require('../src/Customer');
 
 import {
   sampleBookings,
@@ -15,4 +16,51 @@ describe('Customer', function() {
     const customer = new Customer(sampleCustomers[0]);
   });
 
+  it('should be a function', function() {
+    expect(Customer).to.be.a('function');
+  });
+
+  it.skip('should be an instance of a Customer', function() {
+    expect(customer).to.be.an.instanceOf(Customer);
+  });
+
+  it.skip('should have a name', function() {
+    expect(customer.name).to.equal('Leatha Ullrich');
+  });
+
+  it.skip('should have an ID', function() {
+    expect(customer.id).to.equal(1);
+  });
+
+  it.skip('should have a username', function() {
+    expect(customer.username).to.equal();
+  });
+
+  it.skip('should have a password', function() {
+    expect(customer.password).to.equal();
+  });
+
+  it.skip('should track the total amount they\'ve spent on rooms', function() {
+    expect(customer.amountSpent).to.equal();
+  });
+
+  it.skip('should have a list of bookings', function() {
+    expect(customer.bookings).to.eql(bookings);
+  });
+
+  it.skip('should have a list of past bookings', function() {
+    expect(customer.pastBookings).to.eql([]);
+  });
+
+  it.skip('should be able to book a room', function() {
+
+  });
+
+  it.skip('should be able to cancel a booking', function() {
+
+  });
+
+  it.skip('should be able to calculate the total amount they\'ve spent on rooms', function() {
+
+  });
 })
