f42844b4875b436ee67a0a5ba69a48d675439497
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-06 19:28:34 -0700

    Conduct room test

--- test/room-test.js
+++ test/room-test.js
@@ -3,54 +3,54 @@ const expect = chai.expect;
 const Room = require('../src/Room');
 
 import {
-  sampleBookings,
+  // sampleBookings,
   sampleRooms,
-  sampleCustomers
+  // sampleCustomers
 } from './sampleData.js';
 
 describe('Room', function() {
   let room;
+  let room2;
 
   beforeEach(function() {
     room = new Room(sampleRooms[0]);
     room2 = new Room(sampleRooms[1]);
   });
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(Room).to.be.a('function');
   });
 
-  it.skip('should be an instance of a Room', function() {
+  it('should be an instance of a Room', function() {
     expect(room).to.be.an.instanceOf(Room);
   });
 
-  it.skip('should have a number', function() {
+  it('should have a number', function() {
     expect(room.number).to.equal(1);
   });
 
-  it.skip('should have a type', function() {
+  it('should have a type', function() {
     expect(room.roomType).to.equal('residential suite');
   });
 
-  it.skip('could or could not have a bidet', function() {
+  it('could or could not have a bidet', function() {
     expect(room.hasBidet).to.equal(true);
     expect(room2.hasBidet).to.equal(false);
   });
 
-  it.skip('should have a bed size', function() {
+  it('should have a bed size', function() {
     expect(room.bedSize).to.equal('queen');
   });
 
-  it.skip('should either be available or unavailable', function() {
+  it('should either be available or unavailable', function() {
     expect(room.isAvailable).to.equal(false);
-    expect(room.isAvailable).to.equal(true);
   });
 
-  it.skip('should have a bed count', function() {
+  it('should have a bed count', function() {
     expect(room.numBeds).to.equal(1);
   });
 
-  it.skip('should have a nightly cost', function() {
+  it('should have a nightly cost', function() {
     expect(room.costPerNight).to.equal(358.40);
   });
 })
