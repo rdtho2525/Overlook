4bda3efdff99c7b28082e7651cc613e5e4369649
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-06 19:08:39 -0700

    Add, import paths to images

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+// import './css/normalize.css'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/pexels-konstantinos-eleftheriadis-2034335.jpg'
+import './images/avatar.svg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
