71f8d053efe9891bb5f45cbbade7c32772979d3f
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-06 19:29:02 -0700

    Add properties to Room class

--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,12 @@
 class Room {
-  constructor() {
-
+  constructor(roomData) {
+    this.number = roomData.number;
+    this.roomType = roomData.roomType;
+    this.hasBidet = roomData.bidet;
+    this.bedSize = roomData.bedSize;
+    this.isAvailable = false;
+    this.numBeds = roomData.numBeds;
+    this.costPerNight = roomData.costPerNight;
   }
 }
 
