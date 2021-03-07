a897535cea48e86151efd3e321ced069ca27e625
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-07 15:42:29 -0700

    Write implementation code for Hotel class

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,6 +1,22 @@
 class Hotel {
-  constructor() {
+  constructor(roomData) {
+    this.availableRooms = roomData;
+  }
 
+  filterRoomsByType(type) {
+    return this.availableRooms.filter(room => room.roomType === type);
+  }
+
+  displayAvailabilityStatus(selectedRoom) {
+    let message;
+    this.availableRooms.forEach(room => {
+      if (room.number === selectedRoom.number) {
+        message = `Wonderful selection - room ${selectedRoom.number} will be ready for you!`;
+      } else {
+        message = `We\'re sorry, room ${selectedRoom.number} is unavailable.  Please refine your search.`
+      }
+    });
+    return message;
   }
 }
 
