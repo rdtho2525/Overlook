8408c5fd65a78bc917ad88258973a7efa23591ed
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-07 15:42:02 -0700

    Conduct tests for Hotel class

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -10,32 +10,63 @@ import {
 
 describe('Hotel', function() {
   let hotel;
+  let newRoom;
 
   beforeEach(function() {
-    hotel = new Hotel();
+    hotel = new Hotel(sampleRooms);
+    newRoom = {
+      "number": 7,
+      "roomType": "junior suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 400.00
+    }
   });
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(Hotel).to.be.a('function');
   });
 
-  it.skip('should be an instance of a Hotel', function() {
+  it('should be an instance of a Hotel', function() {
     expect(hotel).to.be.an.instanceOf(Hotel);
   });
 
-  it.skip('should be have a list of rooms', function() {
-    expect(hotel.rooms).to.eql([]);
+  it('should be have a list of rooms', function() {
+    expect(hotel.availableRooms).to.eql(sampleRooms);
   });
 
-  it.skip('should be able to filter rooms by date', function() {
-    expect(hotel.filterRoomsByDate('', '')).to.eql([/*list of room objs */]);
+  it('should be able to filter rooms by type', function() {
+    expect(hotel.filterRoomsByType('single room')).to.eql([
+      {
+        "number": 3,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 491.14
+      },
+      {
+        "number": 4,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 429.44
+      },
+      {
+        "number": 5,
+        "roomType": "single room",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 2,
+        "costPerNight": 340.17
+      }
+    ]);
   });
 
-  it.skip('should be able to filter rooms by type', function() {
-    expect(hotel.filterRoomsByType('residential suite').to.eql([/*list of room objs */]));
-  });
-
-  it.skip('should inform customer that a room is unavailable', function() {
-    expect(hotel.displayUnavailableStatues()).to.equal('We\'re sorry, room [room details] is unavailable.  Please refine your search.');
+  it('should inform customer of the selected room\'s status', function() {
+    expect(hotel.displayAvailabilityStatus(sampleRooms[5])).to.equal('Wonderful selection - room 6 will be ready for you!');
+    expect(hotel.displayAvailabilityStatus(newRoom)).to.equal('We\'re sorry, room 7 is unavailable.  Please refine your search.');
   });
 })
