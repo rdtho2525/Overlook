57013001c9fd9d4bf533e1cbc6168bc551b54fd7
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-08 14:08:50 -0700

    Add roomSection DOM ele, brainstorm openDashboard

--- src/scripts.js
+++ src/scripts.js
@@ -15,6 +15,7 @@ const juniorButton = document.getElementById('juniorButton');
 const singleRoomButton = document.getElementById('singleRoomButton');
 const pastBookings = document.getElementById('pastBookings');
 const sectionHeader = document.getElementById('sectionHeader');
+const roomSection = document.getElementById('roomSection');
 const roomCard = document.getElementById('roomCard');
 const roomImage = document.getElementById('roomImage');
 const roomNumber = document.getElementById('roomNumber');
@@ -30,10 +31,50 @@ import Room from './Room';
 // const currentCustomer = new Customer();
 // const hotel = new Hotel();
 
+const openDashboard = (roomData) {
+  populateRoomSection(roomData);
+}
+
+const populateRoomSection = (roomData) => {
+  return roomData.forEach(room => {
+    roomSection.innerHTML += `
+  <article id="roomCard" class="room-card">
+    // <div class="room-image">
+    //   <img id="roomImage" src="" alt="">
+    <!-- img id="priceTag" src="" alt=""> -->
+    // </div>
+    <div class="room-details">
+      <img id="" class="booking-icon" src="" alt="Book Room Icon">
+      <p id="roomNumber">${room.number}</p>
+      <p id="roomType">${room.roomType}</p>
+      <p id="bedNum">${room.numBeds}</p>
+      <p id="availabilityStatus">Status: Available</p>
+    </div>
+  </article>`
+  });
+}
+
+const populateBookingsSection = (bookingData) => {
+  return bookingData.forEach(booking => {
+    roomSection.innerHTML += `<article id="roomCard" class="room-card">
+    // <div class="room-image">
+    //   <img id="roomImage" src="" alt="">
+    <!-- img id="priceTag" src="" alt=""> -->
+    // </div>
+    <div class="room-details">
+      <img id="" class="booking-icon" src="" alt="Book Room Icon">
+      <p id="guestID">${booking.userID}</p>
+      <p id="bookingRoom">${booking.roomNumber}</p>
+      <!--<p id=""></p>-->
+      <p id="availabilityStatus">Status: Available</p>
+    </div>
+  </article>`
+  });
+}
+
 
 const fetchCustomers = fetch('http://localhost:3001/api/v1/customers')
   .then(response => response.json())
-  // .then(customer => console.log(customer))
   .catch(err => console.log(err));
 
 // const fetchSingleCustomer = fetch('http://localhost:3001/api/v1/customers/<id> where<id> will be a number of a customer’s id')
@@ -42,12 +83,10 @@ const fetchCustomers = fetch('http://localhost:3001/api/v1/customers')
 
 const fetchBookings = fetch('http://localhost:3001/api/v1/bookings')
   .then(response => response.json())
-  // .then(booking => console.log(booking))
   .catch(err => displayErrorMessage(err));
 
 const fetchRooms = fetch('http://localhost:3001/api/v1/rooms')
   .then(response => response.json())
-  // .then(room => console.log(room))
   .catch(err => displayErrorMessage(err));
 
 Promise.all([fetchCustomers, fetchBookings, fetchRooms])
