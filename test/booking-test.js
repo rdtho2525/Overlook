33bc852d8afde904b9a4b26e86f0e8e5331a4dfc
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-08 11:19:56 -0700

    Remove unnecessary test, add room argument to booking instantiation

--- test/booking-test.js
+++ test/booking-test.js
@@ -3,15 +3,15 @@ const expect = chai.expect;
 const Booking = require('../src/Booking');
 
 import {
-  // sampleBookings,
-  sampleCustomers
+  sampleCustomers,
+  sampleRooms
 } from './sampleData.js';
 
 describe('Booking', function() {
   let booking;
 
   beforeEach(function() {
-    booking = new Booking(sampleCustomers[1], '2020/04/15', 5);
+    booking = new Booking(sampleCustomers[1], '2020/04/15', sampleRooms[4]);
   });
 
   it('should be a function', function() {
@@ -20,10 +20,6 @@ describe('Booking', function() {
 
   it('should be an instance of Booking', function() {
     expect(booking).to.be.an.instanceOf(Booking);
-  });
-
-  it.skip('should have an ID', function() {
-    expect(booking.id).to.equal(/*string*/);
   });
 
   it('should have a user ID', function() {
