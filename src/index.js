349cc04d5e3f3a887595ea4384a273df8215bfc3
Reggie Thompson <reggie.thompson4@gmail.com>
2021-03-08 14:34:38 -0700

    Import booking image

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import './css/base.scss';
 // import './css/normalize.css'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// import './images'
 import './images/turing-logo.png'
 import './images/pexels-konstantinos-eleftheriadis-2034335.jpg'
 import './images/avatar.svg'
+import './images/noun_booking_1094614.svg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
